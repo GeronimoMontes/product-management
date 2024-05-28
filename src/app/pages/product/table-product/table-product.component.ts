4056b850e7ff0bd0518140096b3364b271fe35da
GeronimoMontes <geronimo@adroitoverseas.com>
2024-05-27 17:56:05 -0700

    fetch products (pending)

--- src/app/pages/product/table-product/table-product.component.ts
+++ src/app/pages/product/table-product/table-product.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { IProduct, ProductData } from '../../../@core/data/productoModel';
 
 @Component({
   selector: 'app-table-products',
   templateUrl: './table-product.component.html',
   styleUrls: ['./table-product.component.css'],
 })
-export class TableProductComponent  {
+export class TableProductComponent implements OnInit, OnDestroy {
+  constructor(protected readonly productService: ProductData) {}
+
+  ngOnInit() {
+    this.productService.getAllProducts$().subscribe((data: IProduct[]) => {
+      this.loadingData = !this.loadingData;
+      this.products = data;
+      this.loadingData = !this.loadingData;
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  get loading$(): Observable<boolean> {
+    return new Observable((obs) => obs.next(this.loadingData));
+  }
+
+  private destroy$: Subject<void> = new Subject<void>();
+  public loadingData: boolean = false;
+  protected products: IProduct[] = [];
 }
