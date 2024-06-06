b7bee9885b4066b188076a16cc4a2597c37674e6
GeronimoMontes <geronimo@adroitoverseas.com>
2024-06-06 13:23:36 -0700

    cypress config project tests

---
+++ src/app/@theme/components/sidebar/sidebar.component.cy.ts
@@ -0,0 +1,9 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SidebarComponent } from './sidebar.component';
+
+describe('SidebarComponent', () => {
+  it("Does not do much!", ()=> {
+    expect(true).to.equal(true)
+  })
+});
