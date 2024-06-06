87e9261d8263f2b1e8c741e48178b70c444915e7
GeronimoMontes <geronimo@adroitoverseas.com>
2024-06-05 17:32:42 -0700

    searchService on search asyc data

--- src/app/@theme/components/sidebar/sidebar.component.ts
+++ src/app/@theme/components/sidebar/sidebar.component.ts
@@ -1,8 +1,15 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
 import { AuthService } from '../../../@core/root/auth.service';
 
+
+export interface MenuSidebar {
+  icon: string;
+  action: any;
+  text: string;
+  childs: MenuSidebar[] | undefined | null
+}
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -14,28 +21,29 @@ export class SidebarComponent implements OnInit, OnDestroy {
   constructor(
     protected readonly router: Router,
     protected readonly authService: AuthService
-  ) {}
+  ) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   ngOnDestroy() {
     this.destroy$.next();
     this.destroy$.complete();
   }
 
-  navigateHome() {
-    this.router.navigateByUrl('pages/products/table');
+  navigate(url: string) {
+    if (url.includes('logout')) {
+      this.logOut()
+      return;
+    }
+
+    this.router.navigateByUrl(url);
     return false;
   }
-
-  navigateProfile() {
-    this.router.navigateByUrl('pages/products/table');
-    return false;
-  }
-
 
   logOut() {
     this.authService.logOut$();
     return false;
   }
+
+  @Input() menuSidebar!: MenuSidebar[];
 }
