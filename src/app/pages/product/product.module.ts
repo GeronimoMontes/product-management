4bda3e1bb1322c7db8c5dfc12c06b9d7608f40a4
GeronimoMontes <geronimo@adroitoverseas.com>
2024-06-19 14:26:30 -0700

    clean components delete, view

--- src/app/pages/product/product.module.ts
+++ src/app/pages/product/product.module.ts
@@ -1,13 +1,11 @@
-import { NgModule } from "@angular/core";
-import { ProductComponent } from "./product.component";
-import { ProductRoutingModule } from "./product-routing.module";
 import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { ThemeModule } from "../../@theme/theme.module";
-import { TableProductComponent } from "./table-product/table-product.component";
-import { ViewProductComponent } from "./view-product/view-product.component";
 import { FormProductComponent } from "./form-product/form-product.component";
-import { DeleteProductComponent } from "./delete-product/delete-product.component";
+import { ProductRoutingModule } from "./product-routing.module";
+import { ProductComponent } from "./product.component";
+import { TableProductComponent } from "./table-product/table-product.component";
 import { TableScrollProductComponent } from "./table-scroll-product/table-scroll-product.component";
 
 @NgModule({
@@ -22,8 +20,6 @@ import { TableScrollProductComponent } from "./table-scroll-product/table-scroll
     ProductComponent,
     TableProductComponent,
     TableScrollProductComponent,
-    ViewProductComponent,
-    DeleteProductComponent,
     FormProductComponent,
   ],
 })
