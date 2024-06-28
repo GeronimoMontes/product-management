bf9766305d1c4bc3ff6d3072883cdae9806bd80c
GeronimoMontes <geronimo@adroitoverseas.com>
2024-06-28 16:15:43 -0700

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
+    projectId: 'af6a31f3-8425-4d2e-8657-82754d0677a3',
     baseUrl: 'http://localhost:4200/',
   },
   env: {
